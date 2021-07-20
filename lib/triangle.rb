967f8469859cf36b3adb78a13156a5fd465d209a
amy-chinlai <amy@actionnetwork.org>
2021-07-20 16:40:54 -0400

    commit tree b63f4163a4d632ac0bbccbb48dea69df0059a63f

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,33 @@
+require 'pry'
 class Triangle
-  # write code here
+  
+  def initialize(side_1, side_2, side_3)
+    @side_1 = side_1
+    @side_2 = side_2
+    @side_3 = side_3
+    @sides = [@side_1, @side_2, @side_3]
+  end
+
+  def kind
+    if valid?
+      if @side_1 == @side_2 && @side_2 == @side_3
+        :equilateral
+      elsif @side_1 == @side_2 || @side_2 == @side_3 || @side_1 == @side_3
+        :isosceles
+      elsif @side_1 != @side_2 && @side_2 != @side_3
+        :scalene
+      end
+    else
+      raise TriangleError
+    end
+  end
+
+  def valid?
+    @sides.all? {|side| side > 0} && @side_1 + @side_2 > @side_3 && @side_2 + @side_3 > @side_1 && @side_1 + @side_3 > @side_2
+  end
+
+  class TriangleError < StandardError
+  end
+
+
 end
